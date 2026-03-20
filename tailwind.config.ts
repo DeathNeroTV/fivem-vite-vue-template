14674c22784078b592e2de02620ca08a6e2d9340
DeathNeroTV <runge.roman@gmail.com>
2026-03-20 14:25:27 +0100

    initialize project files

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+
+export default {
+	content: ["./index.html", "./src/**/*.{vue,ts,js,jsx,tsx}"],
+	theme: {
+		extend: {
+			animation: {
+				fadein: "fadeIn .5s ease-in-out",
+				fadeout: "fadeOut .5s ease-in-out",
+			},
+			keyframes: {
+				fadeIn: {
+					from: { opacity: 0 },
+					to: { opacity: 1 },
+				},
+				fadeOut: {
+					from: { opacity: 1 },
+					to: { opacity: 0 },
+				},
+			},
+		},
+	},
+	plugins: [],
+} satisfies Config;
---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+
+export default defineConfig({
+	base: "./",
+	build: {
+		outDir: path.resolve(__dirname, "../dist"),
+		emptyOutDir: true,
+		minify: "esbuild",
+		target: "esnext",
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes("node_modules")) {
+						if (id.includes("vue")) return "vue";
+						return "vendor";
+					}
+				},
+			},
+		},
+	},
+	resolve: {
+		alias: {
+			"@Types": path.resolve(__dirname, "src/types"),
+			"@Composables": path.resolve(__dirname, "src/composables"),
+			"@Components": path.resolve(__dirname, "src/components"),
+		},
+	},
+	plugins: [vue()],
+});
